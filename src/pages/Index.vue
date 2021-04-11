2b5abdb88489c128431bb7a7653ef39f0c9ea930
FORRREST-PC-15\Forrrest <birdinforest@gmail.com>
2021-04-11 12:43:10 +1000

    ci(master): Test CI from PR

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <q-page class="row items-center justify-evenly">
     Home page placeholder.
+    Test PR CI from github.
   </q-page>
   <q-page class="row items-center justify-evenly">
     <example-component
