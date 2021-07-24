484cebecd547740aa29027a5703d328a61710ed2
Derek Wang <birdinforest@gmail.com>
2021-07-24 21:18:39 +1000

    feat(Areas Page): Add buttons, multiple selection, filter. Change data&time format. Misc.

--- src/pages/AreasPage.vue
+++ src/pages/AreasPage.vue
@@ -4,16 +4,39 @@
       <q-table
           :columns="columns"
           :rows="rows"
+          :loading="loading"
+          :filter="filter"
           row-key="name"
-      />
+          :selected-rows-label="getSelectedString"
+          selection="multiple"
+          v-model:selected="selected"
+      >
+        <template v-slot:body-cell-buttons="props">
+          <q-td :props="props">
+            <div>
+              <q-btn color="primary" label="Info/Edit" size="ms" />
+            </div>
+          </q-td>
+        </template>
+
+        <template v-slot:top>
+          <q-input dense debounce="300" color="primary" v-model="filter">
+            <template v-slot:before>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+          <q-space />
+          <q-btn color="primary" size="ms" :disable="loading" label="Add row" @click="addRow" />
+          <q-btn class="q-ml-sm" color="red" size="ms" :disable="loading" label="Remove row" @click="removeRow" />
+        </template>
+      </q-table>
     </div>
   </q-page>
-  <p>Rows count:</p>
-  <p>{{ rowsCount }}</p>
-  <q-btn push color="primary" label="Add" @click="addArea"/>
 </template>
 
 <script lang="ts">
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ref } from 'vue'
 import { Vue, Options, setup } from 'vue-class-component'
 import { pageLog } from '../utility/logger'
 import APIManager, {
@@ -26,8 +49,6 @@ import {
   AreaData, AreaStateInterface,
   moduleNames,
   areaGetterLocalTypes, areaActionLocalTypes,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  GetterLocalTypes, ActionLocalTypes,
   getGlobalType, useStore
 } from '../store'
 
@@ -59,12 +80,6 @@ interface IAreaDataFirebase {
       const $store = useStore()
       const areaState = $store.state[moduleNames.area] as AreaStateInterface
       return areaState.rows
-    },
-    rowsCount (): number {
-      const $store = useStore()
-      const type = getGlobalType(areaGetterLocalTypes.AREAS_COUNT, moduleNames.area)
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      return $store.getters[type] as number
     }
   }
 })
@@ -72,6 +87,12 @@ interface IAreaDataFirebase {
 export default class AreasPage extends Vue {
   apiManager = new APIManager()
   moduleName = moduleNames.area
+
+  _loading = false
+  _filter = ''
+
+  // selected = ref([])
+  _selected: AreaData[] = []
 
   /**
    * Columns data from DB
@@ -90,6 +111,29 @@ export default class AreasPage extends Vue {
 
   get store () {
     return this.$store
+  }
+
+  get loading () { return this._loading }
+
+  get filter () { return this._filter }
+  set filter (keywords: string) { this._filter = keywords }
+
+  get selected (): AreaData[] { return this._selected }
+  set selected (selected: AreaData[]) {
+    this._selected = selected
+  }
+
+  get rowsCount (): number {
+    const store = useStore()
+    const type = getGlobalType(areaGetterLocalTypes.AREAS_COUNT, moduleNames.area)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    console.log(store.getters[type] as number)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    return store.getters[type] as number
+  }
+
+  getSelectedString (): string {
+    return this.selected.length === 0 ? '' : `${this.selected.length} record${this.selected.length > 1 ? 's' : ''} of ${this.rowsCount} records.`
   }
 
   mounted () {
@@ -130,14 +174,24 @@ export default class AreasPage extends Vue {
 
     const type = getGlobalType(areaActionLocalTypes.ADD_AREA, moduleNames.area)
 
+    function formatData (dateIn: Date) {
+      const yyyy = dateIn.getFullYear()
+      const mm = dateIn.getMonth() + 1 // getMonth() is zero-based
+      const dd = dateIn.getDate()
+      const hh = dateIn.getHours()
+      const min = dateIn.getMinutes()
+      const ss = dateIn.getSeconds()
+      return `${yyyy}-${mm}-${dd} ${hh}:${min}:${ss}` // Leading zeros for mm and dd
+    }
+
     // Todo: Type guard of payload of dispatch.
     // Using module IActions's playload type as generic type.
     this.$store.dispatch(type, {
       edgeID: id,
       name: data.Name,
-      floorID: data.EdgeMarkerRef.path,
+      floor: data.EdgeMarkerRef.path,
       assetsCount: data.AssetRefs.length,
-      lastUpdated: data.LastUpdate.toDate().toTimeString()
+      lastUpdated: formatData(data.LastUpdate.toDate())
     })
       .then(result => {
         console.log(result)
@@ -145,6 +199,14 @@ export default class AreasPage extends Vue {
       .catch(e => {
         pageLog.error('Add new area failed.', e)
       })
+  }
+
+  addRow () {
+    console.log('TODO add row')
+  }
+
+  removeRow () {
+    console.log('TODO remove row')
   }
 }
 </script>
